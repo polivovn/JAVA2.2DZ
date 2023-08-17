cf848be6b4b134aea0ef6b3cfc0347d1a67742e0
polivovn <139216581+polivovn@users.noreply.github.com>
2023-08-17 16:49:25 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int current_balance = 100;
+        int currentBalance = 100;
         int replenishment = 1100;
         int bonus = 0;
 
@@ -8,9 +8,9 @@ public class Main {
             bonus = replenishment / 100; // Вычисляем бонусные рубли
         }
 
-        int total_balance = current_balance + replenishment+ bonus ;
+        int total_balance = currentBalance + replenishment+ bonus ;
 
-        System.out.println("Итоговая сумма на счету клиента: " + total_balance );
+        System.out.println("Итоговая сумма на счету клиента: " + totalBalance );
         System.out.println("Количество бонусных рублей: " + bonus );
     }
 }
